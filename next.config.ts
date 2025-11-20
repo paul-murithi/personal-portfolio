bf7d60e5bf3d87654dd32fecec7d38907927de63
paul-murithi <paulmurithi518@gmail.com>
2025-11-20 09:04:43 +0300

    fix url path issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/personal-portfolio",
+  assetPrefix: "/personal-portfolio/",
   images: {
     unoptimized: true,
   },
